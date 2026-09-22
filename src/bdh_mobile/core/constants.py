e52207b463db4d019edbdbf62e840024a5a8ff25
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-09-23 00:05:18 +0530

    Update constants.py

---
+++ src/bdh_mobile/core/constants.py
@@ -0,0 +1,13 @@
+"""
+Constants for bdh-mobile.
+"""
+
+APP_NAME = "bdh-mobile"
+APP_VERSION = "0.1.0"
+
+# Supported Android Brands
+SUPPORTED_BRANDS = ["samsung", "xiaomi", "vivo", "oppo"]
+
+# Default Paths
+DEFAULT_LOG_DIR = "logs/"
+DEFAULT_REPORT_DIR = "reports/"
